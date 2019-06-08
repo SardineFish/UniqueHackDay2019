208d7b734bded7cab8ba3687f88fb9803be82c6b
SardineFish <Sardine@live.cn>
2019-06-08 20:52:09 +0800

    Add grass burning fx

--- Assets/Script/MapItem/GrassItem.cs
+++ Assets/Script/MapItem/GrassItem.cs
@@ -4,8 +4,32 @@ using System.Collections;
 public class GrassItem : MapItem
 {
     public GameObject Fire;
+    public Sprite BurntGrass;
+    public Sprite NormalGrass;
+    public float BurnTime = 2;
+    public float GrowTime = 2;
+    bool burnt = false;
     public override void OnPlayerTouch()
     {
-        Instantiate(Fire, transform.position, Quaternion.identity);
+        if (burnt)
+            return;
+        burnt = true;
+        StartCoroutine(UnnamedFunc());
+    }
+    IEnumerator UnnamedFunc()
+    {
+        var fx = Instantiate(Fire, transform.position, Quaternion.identity);
+        foreach(var t in Utility.Timer(BurnTime))
+        {
+            yield return null;
+        }
+        GetComponentInChildren<SpriteRenderer>().sprite = BurntGrass;
+        Destroy(fx);
+        foreach (var t in Utility.Timer(GrowTime))
+        {
+            yield return null;
+        }
+        GetComponentInChildren<SpriteRenderer>().sprite = NormalGrass;
+        burnt = false;
     }
 }
