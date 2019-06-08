3617446d6691871b238ac4af9409f6cf256b5bac
SardineFish <Sardine@live.cn>
2019-06-08 20:21:36 +0800

    Add grass block.

---
+++ Assets/Script/MapItem/GrassItem.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrassItem : MapItem
+{
+    public GameObject Fire;
+    public override void OnPlayerTouch()
+    {
+        Instantiate(Fire, transform.position, Quaternion.identity);
+    }
+}
