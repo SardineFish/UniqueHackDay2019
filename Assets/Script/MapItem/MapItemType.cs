48fbe602061ce21dbc16b2336feda2072cc0d68d
CurryPseudo <jesonxiake@126.com>
2019-06-08 15:29:17 +0800

    Add MapItem base script

---
+++ Assets/Script/MapItem/MapItemType.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapItemType : MonoBehaviour
+{
+    public enum TypeEnum
+    {
+        StoneWall, //石墙
+        MapBorder, //地图边缘
+        GrassWall, //草墙
+    }
+    public TypeEnum type;
+}
