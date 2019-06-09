9d870f5e5d58f52a8f0ba93c59527cbe4b3414c0
SardineFish <Sardine@live.cn>
2019-06-09 10:57:35 +0800

    Add depth of field

---
+++ Assets/Script/Camera/CameraPlugin.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class CameraPlugin : MonoBehaviour
+{
+    public bool Enabled = true;
+    private void Update()
+    {
+        
+    }
+}
