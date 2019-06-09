2a184ac368d03e0522ca5e0fb306c7fbe19cc1e6
SardineFish <Sardine@live.cn>
2019-06-09 09:03:43 +0800

    Fix bugs of grass grow.

--- Assets/Script/MapItem/GrassItem.cs
+++ Assets/Script/MapItem/GrassItem.cs
@@ -23,7 +23,8 @@ public class GrassItem : MonoBehaviour
     IEnumerator BurnProcess(float startTime)
     {
         var fx = Instantiate(Fire, transform.position, Quaternion.identity);
-        GetComponentInChildren<SpriteRenderer>().sprite = null;
+        var sprite = GetComponentInChildren<SpriteRenderer>();
+        sprite.enabled = false;
         foreach (var t in Utility.Timer(.5f * BurnTime))
         {
             yield return null;
@@ -37,6 +38,7 @@ public class GrassItem : MonoBehaviour
         {
             yield return null;
         }
+        sprite.enabled = true;
         collider.enabled = true;
         //GetComponentInChildren<SpriteRenderer>().sprite = NormalGrass;
         burnt = false;
